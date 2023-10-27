12adc82ee9fafcd2ced097c3c8db0f3c154ce96f
Yedukondalu Poleboyina <116720252+PoleboyinaYedukondalu@users.noreply.github.com>
2023-10-27 17:51:58 +0530

    Add files via upload

---
+++ methods of arrays.js
@@ -0,0 +1,52 @@
+// empty arry :
+let Myarry =[];
+
+// standed arry :
+let myarray = [1,2,3,4,5];
+
+// object arry :
+let myarray2 = [{user:'nani',role:'jascript'}];
+
+// dynamic arry :
+let my_arry1=[];
+my_arry1.push(1);
+my_arry1.pop(1);
+console.log(my_arry1)
+
+// nested arry :
+let myarry3 =[[1,2,3],[4,5,6]];
+console.log(myarry3)
+console.log(myarry3.slice(1))
+
+// mixed arry :
+let myarray4 =['nani',{company:'clariont',role:'developer'},[9912542198,10],true];
+
+
+// string array :
+let myarry5 =['nani','dhurga','pawan'];
+
+// methods of arrys :
+push
+pop
+shift
+unshift
+replace
+slice
+
+let my_arry = [];
+
+// push :
+my_arry = my_arry.push('nani','clariont','developer');
+
+// pop :
+let myarry = myarry.pop('developer');
+
+// unshift :
+myarry.unshift(1);
+
+// shift :
+myarry.shift();
+
+// slice :
+let myarry1 =[1,2,3,4,5];
+myarry1.slice(1,5);
