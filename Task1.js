ad22e142d0f9d38ae5dd58d3049da55246fa67d5
Yedukondalu Poleboyina <116720252+PoleboyinaYedukondalu@users.noreply.github.com>
2023-10-27 11:48:39 +0530

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,11 @@
+//print only vowels in the statement //
+
+let user = "this is Nani Poleboyina"
+for(let k in user){
+    let c =user[k];
+    if( c ==='a'|| c ==='e'|| c==='i'||c ==='o'||c ==='u' ){
+        console.log(c);
+    }
+
+   
+}
